c98cc68089f0919322d3ead6222760e42c09f167
Mary <Anymail@mail.com>
2025-09-29 17:02:22 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the numb of elements in the array: ");
+        int n = in.nextInt();
+        System.out.println("Enter the elements: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+
+        int num = arr[0];
+        int k = 0;
+        for (int i = 0; i < n; i++){
+            if (k == 0){
+               num = arr[i];
+               k++;
+            }
+            else {
+                if (arr[i] == num){
+                    k++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+       System.out.println(num);
+
     }
 }
